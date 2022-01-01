7bcc10bcf4a13259157a312048a868155b376d85
Odell Purifoy <odell.purifoy@freshly.com>
2022-01-01 18:53:42 -0500

    User model configured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name years_of_experience is_admin preferred_language will_pair username])
+  end
 end
